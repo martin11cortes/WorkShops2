6ab04ae5ad889d3c1cf4f8dc89bc0a29c47f4401
Martin Cortes <martinc2103@hotmail.com>
2018-09-02 20:25:34 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/konrad/workshops/HelloWorldActivityFragment.java
@@ -0,0 +1,22 @@
+package co.edu.konrad.workshops;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class HelloWorldActivityFragment extends Fragment {
+
+    public HelloWorldActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hello_world, container, false);
+    }
+}
